22402e0dc07de085a8efa8c5a404549bdc5783be
karasikrus <karasikrus@gmail.com>
2019-04-06 09:18:54 +0300

    button, form tag

--- js/init.js
+++ js/init.js
@@ -6,7 +6,3 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.materialboxed');
-  var instances = M.Materialbox.init(elems, options);
-});
